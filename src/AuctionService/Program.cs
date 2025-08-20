586bcce13dd8120844ce9a371951157efc540a66
corbi <corbi@HJF>
2025-08-20 17:23:05 +0200

    Aggiungere i file di progetto.

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,37 @@
+using AuctionService.Data;
+using AuctionService.RequestHelpers;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+builder.Services.AddAutoMapper(cfg => { }, typeof(MappingProfiles));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+app.UseAuthorization();
+app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex);
+}
+app.Run();
+
+
---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers
+{
+    public class MappingProfiles:Profile
+    {
+        public MappingProfiles() 
+        {
+            CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+            CreateMap<Item, AuctionDto>().IncludeMembers();
+            CreateMap<CreateAuctionDto, Auction>()
+                .ForMember(dest => dest.Item, source => source.MapFrom(s => s));
+            CreateMap<CreateAuctionDto, Item>();
+        }
+    }
+}
